1e9f433fc9443ef2a65bd1a630e8e36dde4fe9d5
gabby <gabbypaylaga@gmail.com>
2023-12-21 04:10:02 +0800

    feat: added routes

---
+++ client/src/utils/Routes.tsx
@@ -0,0 +1,16 @@
+import Login from '@/pages/Login';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+export const RoutesWrapper = () => {
+	return (
+		<>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+				</Routes>
+			</HashRouter>
+		</>
+	);
+};
+
+export default RoutesWrapper;
