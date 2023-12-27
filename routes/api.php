2e5d67c5e39ac37c3504acda915bbe02b34d2d4e
Ringo <prensz26@gmail.com>
2023-12-27 12:27:31 +0000

    Test Supplier

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/suppliers', 'SupplierController@index')->name('supplier.index');
