e395a57b22549638200f550ccb266568242abc99
Gab <gabbypaylaga@gmail.com>
2023-12-29 19:39:48 +0800

    feat: added navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import { NavIcons } from './NavIcons';
+import { HiUser } from 'react-icons/hi2';
+import { GoHomeFill } from 'react-icons/go';
+import { IoNotifications } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { Inputbox } from '../Inputbox';
+
+export const Navbar = () => {
+	return (
+		<div className="navbar-container flex flex-row items-center justify-between">
+			<Inputbox
+				variant={'searchbar'}
+				buttonIcon={'outside'}
+				placeholder="Search"
+			/>
+			<div className="nav-icon flex flex-row items-center justify-center gap-x-5">
+				<Link to="/Dashboard">
+					<NavIcons
+						icon={
+							<GoHomeFill className="text-primary-dark-gray text-xl" />
+						}
+						title={'Home'}
+					/>
+				</Link>
+				<Link to="/Notifications">
+					<NavIcons
+						icon={
+							<IoNotifications className="text-primary-dark-gray text-xl" />
+						}
+						title={'Notification'}
+					/>
+				</Link>
+				<NavIcons
+					icon={<HiUser className="text-primary-dark-gray text-xl" />}
+					title={'Account'}
+					dropdown={true}
+				/>
+			</div>
+		</div>
+	);
+};
