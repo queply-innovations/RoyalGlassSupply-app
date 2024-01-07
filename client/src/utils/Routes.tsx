d193f067acad684ba6991c32beb2f269d30a803f
Gab <gabbypaylaga@gmail.com>
2024-01-07 20:33:28 +0800

    feat(routes): added userSale page

--- client/src/utils/Routes.tsx
+++ client/src/utils/Routes.tsx
@@ -2,6 +2,7 @@ import Dashboard from '@/pages/Dashboard';
 import Page404 from '@/pages/Page404';
 import Transfer from '@/pages/Transfer';
 import { UserInfo } from '@/pages/User/UserInfo';
+import { UserSales } from '@/pages/User/UserSales';
 import Warehouse from '@/pages/Warehouse';
 import Login from '@pages/Login';
 import { HashRouter, Route, Routes } from 'react-router-dom';
@@ -15,6 +16,7 @@ export const RoutesWrapper = () => {
 					<Route path="*" element={<Page404 />} />
 					<Route path="/Dashboard" element={<Dashboard />} />
 					<Route path="/user/information" element={<UserInfo />} />
+					<Route path="/user/sales" element={<UserSales />} />
 					<Route path="/Warehouse" element={<Warehouse />} />
 					<Route path="/transfer" element={<Transfer />} />
 				</Routes>
