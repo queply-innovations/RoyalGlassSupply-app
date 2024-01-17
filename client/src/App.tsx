7e6f09210f932e48484dce6a190abc8baf039c78
Gab <gabbypaylaga@gmail.com>
2024-01-18 04:29:23 +0800

    test(app): redo routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,56 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import RoutesWrapper from '@utils/Routes';
+import { UserInfo } from '@/pages/User/UserInfo';
+import Dashboard from '@/pages/Dashboard';
+import Login from '@/pages/Login';
+import Page404 from '@/pages/Page404';
+import { PendingInventory } from '@/pages/Pending/PendingInventory';
+import { PendingReturn } from '@/pages/Pending/PendingReturn';
+import { UserSales } from '@/pages/User/UserSales';
+import { PendingTransfer } from '@/pages/Pending/PendingTransfer';
+import { Transaction } from '@/pages/Transactions/Transaction';
+import { Expenses } from '@/pages/Transactions/Expenses';
+import { Reports } from '@/pages/Reports';
+import { Finance } from '@/pages/Finance';
+import { Inventory } from '@/pages/Inventory';
+import { Return } from '@/pages/Return';
+import { Products } from '@/pages/Products/Products';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 const queryClient = new QueryClient();
+
+const routesConfig = [
+	{ path: '/', element: <Login /> },
+	{ path: '*', element: <Page404 /> },
+	{ path: '/dashboard', element: <Dashboard /> },
+	{ path: '/user/information', element: <UserInfo /> },
+	{ path: '/user/sales', element: <UserSales /> },
+	{ path: '/pending/inventory', element: <PendingInventory /> },
+	{ path: '/pending/return', element: <PendingReturn /> },
+	{ path: '/pending/transfer', element: <PendingTransfer /> },
+	{ path: '/transaction', element: <Transaction /> },
+	{ path: '/transaction/expenses', element: <Expenses /> },
+	{ path: '/return', element: <Return /> },
+	{ path: '/inventory', element: <Inventory /> },
+	{ path: '/finance', element: <Finance /> },
+	{ path: '/reports', element: <Reports /> },
+	{ path: '/products', element: <Products /> },
+
+	// ... other routes
+];
 
 function App() {
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
-				<RoutesWrapper />
-				<ReactQueryDevtools />
+				<HashRouter>
+					<Routes>
+						{routesConfig.map(({ path, element }) => (
+							<Route key={path} path={path} element={element} />
+						))}
+					</Routes>
+					<ReactQueryDevtools />
+				</HashRouter>
 			</QueryClientProvider>
 		</>
 	);
