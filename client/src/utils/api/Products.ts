b0da2999c128bdd9924a477f9b4969ce22098366
Gab <gabbypaylaga@gmail.com>
2024-01-19 15:49:09 +0800

    feat(api): added products api

--- client/src/utils/api/Products.ts
+++ client/src/utils/api/Products.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Product } from '@/entities/Products';
 import axios from 'axios';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
@@ -7,7 +8,7 @@ const API_BASE_URL = 'https://65956d2504335332df82b67a.mockapi.io/rgs/api';
 const fetchProducts = async (query = ''): Promise<Product[]> => {
 	await new Promise(resolve => setTimeout(resolve, 100));
 	const response = await axios.get(`${API_BASE_URL}/products`);
-	console.log('fetched warehouses');
+	console.log('fetched products');
 	return response.data;
 };
 
@@ -17,4 +18,17 @@ export const useProducts = () => {
 		queryFn: () => fetchProducts(),
 		refetchOnWindowFocus: false,
 	});
+};
+
+export const addProduct = (data: any) => {
+	const response = axios.post(`${API_BASE_URL}/products`, data);
+	return response;
+};
+export const removeProduct = (id: number) => {
+	const response = axios.delete(`${API_BASE_URL}/products/${id}`);
+	return response;
+};
+export const updateProduct = (data: any) => {
+	const response = axios.delete(`${API_BASE_URL}/products/${data.id}`, data);
+	return response;
 };
