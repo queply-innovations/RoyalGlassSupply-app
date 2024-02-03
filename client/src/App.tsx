ffee35f1f586c2566fc5a6c7209c48c6fa5caed5
gabby <gabbypaylaga@gmail.com>
2024-02-03 08:27:47 +0800

    feat(routes): added provider and routes for app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,11 @@
-import {
-	Login,
-	Finance,
-	Inventory,
-	Reports,
-	Return,
-	Supplier,
-	Page404,
-	Dashboard,
-	Warehouse,
-	UserInfo,
-	UserSales,
-	Transaction,
-	Expenses,
-	PendingInventory,
-	PendingReturn,
-	PendingTransfer,
-	Products,
-} from '@/pages';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { LoginTest } from './pages/Login/Login';
-
-const routesConfig = [
-	{ path: '/', element: <LoginTest /> },
-	{ path: '*', element: <Page404 /> },
-	{ path: '/dashboard', element: <Dashboard /> },
-	{ path: '/user/information', element: <UserInfo /> },
-	{ path: '/user/sales', element: <UserSales /> },
-	{ path: '/pending/inventory', element: <PendingInventory /> },
-	{ path: '/pending/return', element: <PendingReturn /> },
-	{ path: '/pending/transfer', element: <PendingTransfer /> },
-	{ path: '/transaction', element: <Transaction /> },
-	{ path: '/transaction/expenses', element: <Expenses /> },
-	{ path: '/return', element: <Return /> },
-	{ path: '/inventory', element: <Inventory /> },
-	{ path: '/finance', element: <Finance /> },
-	{ path: '/reports', element: <Reports /> },
-	{ path: '/warehouse', element: <Warehouse /> },
-	{ path: '/products', element: <Products /> },
-	{ path: '/supplier', element: <Supplier /> },
-];
-
-const queryClient = new QueryClient();
+import { AppProvider } from './provider/app';
+import AppRoutes from './routes';
 
 function App() {
 	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<HashRouter>
-					<Routes>
-						{routesConfig.map(({ path, element }) => (
-							<Route key={path} path={path} element={element} />
-						))}
-					</Routes>
-					<ReactQueryDevtools />
-				</HashRouter>
-			</QueryClientProvider>
-		</>
+		<AppProvider>
+			<AppRoutes />
+		</AppProvider>
 	);
 }
 
