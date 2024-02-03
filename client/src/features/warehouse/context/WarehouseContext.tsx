0a0782732d2dca84060a95e1adecf7c59c775736
gabby <gabbypaylaga@gmail.com>
2024-02-03 08:30:11 +0800

    file directory changes: arrange and generalize each feature under features folder

---
+++ client/src/features/warehouse/context/WarehouseContext.tsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from 'react';
+
+import { Warehouse } from '../types';
+
+export const WarehouseContext = createContext<Warehouse[] | undefined>(
+	undefined,
+);
+
+export function useWarehouseContext() {
+	const warehouse = useContext(WarehouseContext);
+
+	if (!warehouse) {
+		throw new Error(
+			'useWarehouseContext must be used within WarehouseContext',
+		);
+	}
+	return warehouse;
+}
