315a1a9699a66c4d04a3998dbb108fdf6f1ee58d
gabby <gabbypaylaga@gmail.com>
2024-02-19 22:29:00 +0800

    refactor(warehouse): change layout to mainlayout in warehousePage

--- client/src/pages/__test__/WarehousePage.tsx
+++ client/src/pages/__test__/WarehousePage.tsx
@@ -4,6 +4,7 @@ import WarehouseForm from '@/features/warehouse/__test__/components/WarehouseFor
 import { WarehouseTable } from '@/features/warehouse/__test__/components/WarehouseTable';
 import { WarehouseProvider } from '@/features/warehouse/__test__/context/WarehouseContext';
 import { CommonLayout } from '@/layouts/CommonLayout';
+import { MainLayout } from '@/layouts/MainLayout';
 import { useModal } from '@/utils/Modal';
 // import { MainLayout } from '@/layouts/MainLayout';
 
@@ -11,7 +12,7 @@ export const Warehouse = () => {
 	const { isOpen, openModal, closeModal } = useModal();
 	return (
 		<>
-			<CommonLayout title="Warehouse">
+			<MainLayout title="Warehouse">
 				<WarehouseProvider>
 					<div className="flex flex-auto flex-col gap-5 rounded-lg border border-black/10 bg-white p-5">
 						<div className="flex flex-row justify-between">
@@ -39,7 +40,7 @@ export const Warehouse = () => {
 						<WarehouseForm onClose={closeModal} />
 					</ModalTest>
 				</WarehouseProvider>
-			</CommonLayout>
+			</MainLayout>
 		</>
 	);
 };
