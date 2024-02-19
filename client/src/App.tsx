95c92711482fdcc6a33c390d363bdb1d39321c27
gabby <gabbypaylaga@gmail.com>
2024-02-19 21:22:14 +0800

    refactor(routes): remove unused code and add test route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,14 @@
 import { AppProvider } from './provider/app';
 // import AppRoutes from './routes';
 import AppRouter from './routes/root';
-import { TestRoute } from './routes/testRoute';
+// import { TestRoute } from './routes/testRoute';
 
 function App() {
 	return (
 		<AppProvider>
-			{/* <AppRoutes /> */}
-			{/* Error in protectedRoutes in routes/index.tsx */}
-
-			{/* <AppRouter /> */}
-			{/* uncomment this to preview the app routes */}
-
-			{/*
-            uncomment this to preview the app routes
-            */}
+			{/* uncomment this to preview the app routes*/}
 			<AppRouter />
-
-			{/*
-            added testroutes to bypass auth
-            since auto refresh ang auth kung naa changes
-            source file
-
-            //! PANG TEST LANG NI
-            */}
+			//! PANG TEST LANG NI
 			{/* <TestRoute /> */}
 		</AppProvider>
 	);
