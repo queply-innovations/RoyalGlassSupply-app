e983923fd70f907fccc1cd7bad4193a38ae6e2e3
gabby <gabbypaylaga@gmail.com>
2024-02-21 14:14:14 +0800

    refactor(WarehouseTable): component to use openModal function

--- client/src/features/warehouse/__test__/components/WarehouseTable.tsx
+++ client/src/features/warehouse/__test__/components/WarehouseTable.tsx
@@ -2,20 +2,36 @@ import { useWarehouse } from '../context/WarehouseContext';
 import { Button } from '@/components';
 import { useState } from 'react';
 import { FaPencilAlt } from 'react-icons/fa';
+import { Warehouse } from '../types';
 
 interface WarehouseTableProps {
+	openModal: (data: Warehouse, action: string) => void;
 	isUpdate?: boolean;
 }
 
-export const WarehouseTable = ({ isUpdate = false }: WarehouseTableProps) => {
-	const [update, setUpdate] = useState<boolean>(isUpdate);
+export const WarehouseTable = ({ openModal }: WarehouseTableProps) => {
 	const warehouse = useWarehouse();
+
 	const WarehouseTableHeader = [
 		'Warehouse ID',
+		'Warehouse Code',
 		'Warehouse Name',
 		'Location',
 		'Action',
 	];
+
+	// * This function is used to handle the edit warehouse
+	const handleEditWarehouse = (warehouses: Warehouse) => {
+		console.log('Edit Warehouse Clicked:', warehouses);
+		// * This function will open the modal with the warehouse data
+		openModal(warehouses, 'edit');
+	};
+
+	// * This function is used to handle the remove warehouse
+	const handleRemoveWarehouse = (warehouses: Warehouse) => {
+		console.log('Remove Warehouse Clicked:', warehouses);
+		openModal(warehouses, 'remove');
+	};
 
 	return (
 		<>
@@ -42,22 +58,25 @@ export const WarehouseTable = ({ isUpdate = false }: WarehouseTableProps) => {
 								{warehouse.name}
 							</td>
 							<td className="py-2 text-xs font-normal uppercase">
+								{warehouse.code}
+							</td>
+							<td className="py-2 text-xs font-normal uppercase">
 								{warehouse.location}
 							</td>
 							<td className="flex flex-row justify-center gap-3 py-2 text-xs font-normal uppercase">
 								<Button
 									fill={'empty'}
 									textColor={'black'}
-									//TODO: Add onClick function
-									onClick={() => setUpdate(true)}
+									// * On click of this button, it will pass the warehouse data to the openModal function
+									onClick={() => handleEditWarehouse(warehouse)}
 									className="flex flex-row items-center gap-2"
 								>
 									<FaPencilAlt /> Edit
 								</Button>
 								<Button
 									fill={'red'}
-									//TODO: Add onClick function
-									// onClick={() => handleRemoveWarehouse(warehouse)}
+									// * On click of this button, it will pass the warehouse data to the openModal function
+									onClick={() => handleRemoveWarehouse(warehouse)}
 								>
 									Remove
 								</Button>
