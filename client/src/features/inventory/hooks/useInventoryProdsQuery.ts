58f3af33a1972ae90ae62b752967f41eeaf3bb63
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-03-12 20:40:37 +0800

    feat: added context and query hooks for inventory items/products

--- client/src/features/inventory/hooks/useInventoryProdsQuery.ts
+++ client/src/features/inventory/hooks/useInventoryProdsQuery.ts
@@ -3,22 +3,50 @@ import {
 	fetchInventoryProducts,
 	fetchInventoryProductById,
 } from '../api/Inventory';
+import { useEffect, useState } from 'react';
+import { InventoryProduct } from '../types';
 
 export const useInventoryProductsQuery = () => {
-	const { data, isFetching: isLoading } = useQuery({
+	// State of the response data
+	const [data, setData] = useState<InventoryProduct[]>(
+		[] as InventoryProduct[],
+	);
+
+	// Query for fetching inventory products and isLoading state
+	const { data: result, isFetching: isLoading } = useQuery({
 		queryKey: ['inventoryProducts'],
 		queryFn: () => fetchInventoryProducts(),
 		refetchOnWindowFocus: false,
 	});
 
+	// Update states when query results changes [result, isLoading]
+	useEffect(() => {
+		if (!isLoading && result) {
+			setData(result);
+		}
+	}, [result, isLoading]);
+
 	return { data, isLoading };
 };
 
 export const useInventoryProductByIdQuery = (inventory_id: number) => {
-	const { data, isFetching: isLoading } = useQuery({
+	// State of the response data
+	const [data, setData] = useState<InventoryProduct[]>(
+		[] as InventoryProduct[],
+	);
+
+	const { data: result, isFetching: isLoading } = useQuery({
 		queryKey: ['inventoryProductById', inventory_id],
 		queryFn: () => fetchInventoryProductById(inventory_id),
 		refetchOnWindowFocus: false,
 	});
+
+	// Update states when query results changes [result, isLoading]
+	useEffect(() => {
+		if (!isLoading && result) {
+			setData(result);
+		}
+	}, [result, isLoading]);
+
 	return { data, isLoading };
 };
