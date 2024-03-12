3815e8a4ea46cf97a5fa961375d5b447c592c5bc
gabby <gabbypaylaga@gmail.com>
2024-03-13 04:51:20 +0800

    Add invoice items and update invoice context

--- client/src/features/invoice/__test__/context/InvoiceContext.tsx
+++ client/src/features/invoice/__test__/context/InvoiceContext.tsx
@@ -5,7 +5,7 @@ import {
 	useEffect,
 	useState,
 } from 'react';
-import { Invoices } from '../types';
+import { InvoiceItems, Invoices } from '../types';
 import { useInvoiceQuery } from '../hooks/useInvoiceQuery';
 import { useAuth } from '@/context/AuthContext';
 import { formatUTCDateOnly, getDateNow } from '@/utils/timeUtils';
@@ -17,6 +17,11 @@ interface InvoiceContextProps {
 	invoiceSelected: Invoices;
 	setInvoiceSelected: (invoice: Invoices) => void;
 	generateInvoice: (warehouse: Partial<Warehouse>) => void;
+	invoice: Partial<Invoices>;
+	handleChange: (key: string, value: Invoices[keyof Invoices]) => void;
+	formatCurrency: (value: number) => string;
+	AddInvoiceItems: () => void;
+	invoiceItems: Partial<InvoiceItems>[];
 }
 
 interface InvoiceProviderProps {
@@ -30,24 +35,61 @@ const InvoiceContext = createContext<InvoiceContextProps | undefined>(
 export const InvoiceProvider = ({ children }: InvoiceProviderProps) => {
 	const { auth } = useAuth();
 	const { invoices } = useInvoiceQuery();
-	const { selectedWarehouse, order } = usePos();
+	const { selectedWarehouse, order, selectedProducts } = usePos();
 
 	const [invoiceSelected, setInvoiceSelected] = useState<Invoices>(
 		{} as Invoices,
 	);
+	const [invoiceItems, setInvoiceItems] = useState<Partial<InvoiceItems>[]>(
+		[],
+	);
+	const [invoice, setInvoice] = useState<Partial<Invoices>>({});
 
-	const [invoice, setInvoice] = useState<Partial<Invoices>>({} as Invoices);
+	function handleChange(key: string, value: Invoices[keyof Invoices]) {
+		setInvoice(prev => ({
+			...prev,
+			[key]: value,
+		}));
+	}
 
-	function generateOR() {
-		const date = formatUTCDateOnly(getDateNow());
-		const invoiceNumber = invoices.length + 1;
-		const paddedInvoiceNumber = invoiceNumber.toString().padStart(5, '0');
-		const or_no = `${date}-${auth.user.id}-${paddedInvoiceNumber}`;
-		return or_no;
+	function AddInvoiceItems() {
+		selectedProducts.forEach(product => {
+			setInvoiceItems(prev => [
+				...prev,
+				{
+					invoice_id: invoice.id,
+					product_id: product.product.id,
+					product_price_id: product.product.product?.id,
+					product_price: product.price,
+					quantity: product.quantity,
+					unit: product.product.unit,
+					item_discount: 0,
+					discount_approval_status: '',
+					approved_by: 0,
+					source_inventory: selectedWarehouse.id,
+				},
+			]);
+		});
+	}
+
+	// function generateOR() {
+	// 	const date = formatUTCDateOnly(getDateNow());
+	// 	const invoiceNumber = invoices.length + 1;
+	// 	const paddedInvoiceNumber = invoiceNumber.toString().padStart(5, '0');
+	// 	const or_no = `${date}-${auth.user.id}-${paddedInvoiceNumber}`;
+	// 	return or_no;
+	// }
+
+	function formatCurrency(value: number) {
+		return new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'PHP',
+		}).format(value);
 	}
 
 	function generateInvoice() {
-		setInvoice({
+		setInvoice(prev => ({
+			...prev,
 			issued_by: auth.user.id,
 			id: invoices.length + 1,
 			code: `IVC-${selectedWarehouse.id}-${invoices.length + 1}`,
@@ -56,8 +98,8 @@ export const InvoiceProvider = ({ children }: InvoiceProviderProps) => {
 			created_at: getDateNow(),
 			subtotal: order.totalAmount,
 			total_amount_due: order.totalAmount,
-			or_no: generateOR(),
-		});
+			or_no: '',
+		}));
 		return invoice;
 	}
 
@@ -66,8 +108,28 @@ export const InvoiceProvider = ({ children }: InvoiceProviderProps) => {
 		invoiceSelected,
 		setInvoiceSelected,
 		generateInvoice,
+		invoice,
+		handleChange,
+		formatCurrency,
+		invoiceItems,
+		AddInvoiceItems,
 	};
-
+	useEffect(() => {
+		setInvoice({
+			...invoice,
+			issued_by: auth.user.id,
+			id: invoices.length + 1,
+			code: `IVC-${selectedWarehouse.id}-${invoice?.id}`,
+			warehouse_id: selectedWarehouse.id,
+			total_amount_due: 0,
+			change_amount: 0,
+			subtotal: 0,
+			payment_method: '',
+			reference_no: '',
+			paid_amount: 0,
+			total_discount: 0,
+		});
+	}, []);
 	return (
 		<InvoiceContext.Provider value={value}>
 			{children}
