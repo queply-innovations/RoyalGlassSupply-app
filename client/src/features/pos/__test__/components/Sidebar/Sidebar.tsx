382cbbe34635acc53d6dda0e1acf8f4328b46f97
gabby <gabbypaylaga@gmail.com>
2024-03-22 23:52:13 +0800

    Add invoice functionality to Sidebar component

--- client/src/features/pos/__test__/components/Sidebar/Sidebar.tsx
+++ client/src/features/pos/__test__/components/Sidebar/Sidebar.tsx
@@ -3,12 +3,29 @@ import { useAuth } from '@/context/AuthContext';
 import { usePos } from '../../context/__test__/PosContext';
 import { SearchCustomer } from '@/features/customer/__test__/components/SearchCustomer';
 import { CustomerInfo } from './Customer/CustomerInfo';
+import { Button } from '@/components/ui/button';
+import { useInvoice } from '@/features/invoice/__test__/context/InvoiceContext';
+import { TotalItems } from './TotalItems';
+import { Payment } from './Payment';
+import { useInvoiceMutation } from '@/features/invoice/__test__/hooks/useInvoiceMutation';
+import { useCustomer } from '@/features/customer/__test__/context/CustomerContext';
 
 interface SidebarProps {}
 
 export const Sidebar = ({}: SidebarProps) => {
 	const { auth } = useAuth();
+
 	const { setFilter } = usePos();
+
+	const { invoiceItemsQueue, invoice } = useInvoice();
+	const { addInvoiceMutation } = useInvoiceMutation();
+
+	const { selectedCustomer } = useCustomer();
+
+	function handlePrint() {
+		console.log('Invoice:', invoice);
+		console.log('InvoiceItems:', invoiceItemsQueue);
+	}
 	return (
 		<div className="bg-pos-primary-background flex w-full max-w-[350px] flex-col gap-2 p-4">
 			{auth.role === 'admin' ? (
@@ -33,8 +50,7 @@ export const Sidebar = ({}: SidebarProps) => {
 						<TabsList className="flex flex-row">
 							<TabsTrigger
 								value="CDO"
-								className="data-[state=active]:bg-primary w-full rounded-md
-	                            text-slate-800 hover:bg-slate-200 data-[state=active]:text-white"
+								className="data-[state=active]:bg-primary w-full rounded-md text-slate-800 hover:bg-slate-200 data-[state=active]:text-white"
 							>
 								CDO
 							</TabsTrigger>
@@ -57,8 +73,34 @@ export const Sidebar = ({}: SidebarProps) => {
 			</Button> */}
 			<SearchCustomer />
 			<CustomerInfo />
-			{/* <TotalItems />
-			<Payment /> */}
+			<TotalItems />
+			<Payment />
+			<Button
+				onClick={() => {
+					console.log(invoiceItemsQueue);
+				}}
+			>
+				TEST INVOICE ITEMS
+			</Button>
+			<Button
+				onClick={() => {
+					handlePrint();
+				}}
+			>
+				Print Invoice
+			</Button>
+			<Button
+				onClick={() => {
+					addInvoiceMutation(invoice);
+				}}
+				disabled={
+					Object.keys(selectedCustomer).length === 0 ||
+					invoiceItemsQueue.length === 0 ||
+					invoice.paid_amount === 0
+				}
+			>
+				Submit
+			</Button>
 		</div>
 	);
 };
