90328430be6bd920fc549e8fd0e66280778336c9
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-04-05 00:23:45 +0800

    feat: add generateInventoryCode helper function
    - automatically generate inventory code when adding one
    - update inventory code on inventory edit

--- client/src/pages/__test__/InventoryPage.tsx
+++ client/src/pages/__test__/InventoryPage.tsx
@@ -20,6 +20,7 @@ import { EditInventoryForm } from '@/features/inventory/components/forms/EditInv
 export const Inventory = () => {
 	const { warehouses } = useWarehouseQuery();
 	const [filterWarehouse, setFilterWarehouse] = useState(0);
+	const [totalInventories, setTotalInventories] = useState<number>(0);
 
 	// Modal handlers
 	const { openModal, isOpen, closeModal } = useModal();
@@ -72,6 +73,7 @@ export const Inventory = () => {
 									filterWarehouse > 0 ? filterWarehouse : undefined
 								}
 								openModal={modalHandler}
+								setTotalInventories={setTotalInventories}
 							/>
 						</div>
 					</div>
@@ -94,6 +96,7 @@ export const Inventory = () => {
 							{modalAction === 'add' && (
 								<AddInventoryForm
 									warehouses={warehouses}
+									totalInventories={totalInventories}
 									onClose={closeModal}
 								/>
 							)}
