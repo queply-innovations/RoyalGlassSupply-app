84a416f7e2ff90ab96c322458a142911965b4b0e
Gab <gabbypaylaga@gmail.com>
2024-04-19 23:57:59 +0800

    Update API_BASE_URL in Url.ts to use environment variable

--- client/src/api/Url.ts
+++ client/src/api/Url.ts
@@ -1,6 +1,7 @@
 import storage from '@/utils/storage';
 
-export const API_BASE_URL = 'https://staging.royalglasssupply.com/api';
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
+// export const API_BASE_URL = import.meta.env.VITE_API_URL_BUILD; //TODO Dont comment when building
 
 export const API_URLS = {
 	PRODUCTS: `${API_BASE_URL}/products`,
