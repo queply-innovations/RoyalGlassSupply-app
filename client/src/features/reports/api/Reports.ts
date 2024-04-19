c13b373182a8ee9d3b7c5305903e66da80653872
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-04-20 05:40:19 +0800

    refactor: refactor reports components and hooks

---
+++ client/src/features/reports/api/Reports.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { API_HEADERS, API_URLS } from '@/api';
+import { Invoices } from '@/features/invoice/__test__/types';
+
+export const fetchInvoicesDateRange = async (
+	dateFrom: Date,
+	dateTo: Date,
+): Promise<Invoices[]> => {
+	return await axios
+		.post(
+			`${API_URLS.INVOICE}/searches-filters-sorts`,
+			{
+				date_range: {
+					created_at: {
+						from: dateFrom.toISOString().split('T')[0],
+						to: dateTo.toISOString().split('T')[0],
+					},
+					updated_at: {
+						from: dateFrom.toISOString().split('T')[0],
+						to: dateTo.toISOString().split('T')[0],
+					},
+				},
+			},
+			{
+				headers: API_HEADERS(),
+			},
+		)
+		.then(response => {
+			return response.data.data;
+		})
+		.catch(error => {
+			console.error('Error fetching invoices:', error);
+			throw error;
+		});
+};
--- client/src/pages/__test__/ReportsPage.tsx
+++ client/src/pages/__test__/ReportsPage.tsx
@@ -1,46 +1,61 @@
 import GrossAndNetProfit from '@/components/Chart/GrossandNetProfit';
 import { ReportCard, TopProductsCard } from '@/features/reports/components';
 import { MainLayout } from '@/layouts/MainLayout';
+import { DatePickerWithRange } from '@/features/reports/components/DatePickerWithRange';
+import { ReportsProvider } from '@/features/reports';
+import { SalesRevenue } from '@/features/reports/components/SalesRevenue';
+import { ResetToDefaultButton } from '@/features/reports/components/ResetToDefaultButton';
+import { GrossAndNetProfitCard } from '@/features/reports/components/charts/GrossAndNetProfit';
 
 export const Reports = () => {
 	const dateFilters = ['Monthly', 'Quarterly', 'Yearly'];
+
 	return (
 		<>
 			<MainLayout title="Reports">
-				<div className="flex h-full w-full flex-col justify-between gap-5">
-					<div className="flex w-full flex-row justify-between gap-5 ">
-						<div className="flex flex-1 flex-col gap-4">
-							<div className="flex flex-row gap-4">
-								<ReportCard
-									title="Sales Revenue"
-									date
-									formatAmount
-									filter={dateFilters}
-								/>
-								<ReportCard
-									title="Expected Profit"
-									formatAmount
-									filter={dateFilters}
-								/>
-							</div>
-							<div className="flex flex-row gap-4">
-								<ReportCard
-									title="Total Collectibles"
-									date
-									formatAmount
-									filter={dateFilters}
-								/>
-								<ReportCard
-									title="Total Capital"
-									formatAmount
-									filter={dateFilters}
-								/>
+				<ReportsProvider>
+					<div className="flex h-full w-full flex-1 flex-col gap-5 rounded-xl border border-black/10 bg-white p-4">
+						<div className="h-full max-h-full w-full max-w-full overflow-auto">
+							<div className="flex w-full max-w-full flex-col gap-4">
+								<div className="flex flex-row gap-2">
+									<DatePickerWithRange />
+									<ResetToDefaultButton />
+								</div>
+								<div className="flex flex-1 flex-col gap-4">
+									<div className="flex flex-row gap-4">
+										<SalesRevenue />
+										<ReportCard title="Expected Profit">
+											<p className="text-2xl font-bold tracking-tight">
+												{Intl.NumberFormat('en-US', {
+													style: 'currency',
+													currency: 'PHP',
+												}).format(4508)}
+											</p>
+										</ReportCard>
+										<ReportCard title="Collectibles">
+											<p className="text-2xl font-bold tracking-tight">
+												{Intl.NumberFormat('en-US', {
+													style: 'currency',
+													currency: 'PHP',
+												}).format(7129)}
+											</p>
+										</ReportCard>
+										<ReportCard title="Capital">
+											<p className="text-2xl font-bold tracking-tight">
+												{Intl.NumberFormat('en-US', {
+													style: 'currency',
+													currency: 'PHP',
+												}).format(11028)}
+											</p>
+										</ReportCard>
+									</div>
+								</div>
+								<TopProductsCard />
+								<GrossAndNetProfitCard />
 							</div>
 						</div>
-						<TopProductsCard />
 					</div>
-					<GrossAndNetProfit />
-				</div>
+				</ReportsProvider>
 			</MainLayout>
 		</>
 	);
