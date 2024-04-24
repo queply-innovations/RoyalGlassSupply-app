5aaa6fd1dedbc763de2c09deac00af8ab9c9e472
Neil Francis Britos <neilexfrancis18@gmail.com>
2024-04-24 21:54:05 +0800

    UPDATE - remaining stocks update

--- app/Http/Resources/InventoryProductResource.php
+++ app/Http/Resources/InventoryProductResource.php
@@ -17,9 +17,9 @@ class InventoryProductResource extends JsonResource
         $transferred_stocks_count = $this->transferProducts->sum('total_quantity');
         $sold_count = $this->invoiceItems->sum('quantity');
         // $miscellaneous_count = $this->invoiceItems->sum('total_miscellaneous');
-        $remaining_stocks = $this->stocks_count - ($transferred_stocks_count + $sold_count);
+        $remaining_stocks = $this->approved_stocks - ($transferred_stocks_count + $sold_count);
+        // $remaining_stocks = $this->stocks_count - ($transferred_stocks_count + $sold_count);
         $product = new ProductResource($this->product);
-        $productPrices = new ProductPriceCollection($this->product->productPrices);
         $unapprovedStocks = $this->stocks_count - $this->approved_stocks;
 
         return [
@@ -37,9 +37,9 @@ class InventoryProductResource extends JsonResource
             'transferred_stocks_count' => $transferred_stocks_count,
             'sold_count' => $sold_count,
             'miscellaneous_count' => 0,
-            'remaining_stocks_count' => $remaining_stocks,
+            'remaining_stocks_count' => $remaining_stocks > 0 ? $remaining_stocks : 0,
             'status' => $this->approved_stocks > 0 ? $this->status : 0,
-            'inventory' => new InventoryResource($this->inventory)
+            'inventory' => new InventoryResource($this->inventory),
         ];
     }
 }
