be25c2ba7d71ac55de43397b4858eef47ac2bb0f
gabby <gabbypaylaga@gmail.com>
2024-04-26 15:43:00 +0800

    Add POS return invoice components and tables

---
+++ client/src/features/pos/__test__/components/index.ts
@@ -0,0 +1,8 @@
+export * from './Navbar/Navbar';
+export * from './Sidebar/Sidebar';
+
+//Tables
+export * from './Table/CreateOrderTable';
+export * from './Table/EmptyPlaceholder';
+export * from './Table/PosTable';
+export * from './Table/ReturnTable';
