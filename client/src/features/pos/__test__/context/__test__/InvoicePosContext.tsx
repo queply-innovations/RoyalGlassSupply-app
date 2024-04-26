d493d38eabfb5c7da0ccb0baa75ea2ff0b8a7ef1
gabby <gabbypaylaga@gmail.com>
2024-04-27 06:12:31 +0800

    refactor: added invoice subtotal
    -function addInvoiceItem still on testing

--- client/src/features/pos/__test__/context/__test__/InvoicePosContext.tsx
+++ client/src/features/pos/__test__/context/__test__/InvoicePosContext.tsx
@@ -1,16 +1,29 @@
 import { useAuth } from '@/context/AuthContext';
 import {
    InvoiceItemDatabase,
+   InvoiceItems,
    Invoices,
 } from '@/features/invoice/__test__/types';
-import { ReactNode, createContext, useContext, useState } from 'react';
+import {
+   ReactNode,
+   createContext,
+   useContext,
+   useEffect,
+   useMemo,
+   useState,
+} from 'react';
 
 interface InvoicePosContextProps {
    currentInvoicePos: Partial<Invoices>;
    setCurrentInvoicePos: React.Dispatch<Partial<Invoices>>;
 
    currentInvoiceItemsQueue: InvoiceItemDatabase[];
-   setCurrentInvoiceItemsQueue: React.Dispatch<InvoiceItemDatabase[]>;
+   setCurrentInvoiceItemsQueue: (invoiceItems: InvoiceItemDatabase[]) => void;
+
+   addInvoiceItems: (
+      tableIndex: number,
+      item: Partial<InvoiceItemDatabase>,
+   ) => void;
 
    handleInvoicePosChange: (
       key: string,
@@ -53,10 +66,81 @@ export const InvoicePosProvider = ({ children }: InvoiceProviderProps) => {
    } as Invoices);
 
    const [currentInvoiceItemsQueue, setCurrentInvoiceItemsQueue] = useState<
-      InvoiceItemDatabase[]
+      Partial<InvoiceItems>[]
    >([]);
 
-   function addInvoiceItems() {}
+   const invoiceSubtotal = useMemo(() => {
+      return currentInvoiceItemsQueue.reduce(
+         (acc, currentItem) =>
+            acc + (currentItem.total_price ?? 0) * (currentItem.quantity ?? 0),
+         0,
+      );
+   }, [currentInvoiceItemsQueue]);
+
+   useEffect(() => {
+      setCurrentInvoicePos(prev => ({
+         ...prev,
+         subtotal: invoiceSubtotal,
+         total_amount_due:
+            invoiceSubtotal -
+            (prev.total_discount ?? 0) +
+            (prev.delivery_charge ?? 0) +
+            (prev.total_tax ?? 0),
+      }));
+   }, [
+      invoiceSubtotal,
+      currentInvoicePos.total_discount,
+      currentInvoicePos.delivery_charge,
+      currentInvoicePos.total_tax,
+   ]);
+
+   function addInvoiceItems(
+      tableIndex: number,
+      item: Partial<InvoiceItemDatabase>,
+   ) {
+      if (tableIndex !== -1) {
+         const updatedInvoiceItemsQueue = [...currentInvoiceItemsQueue];
+         updatedInvoiceItemsQueue[tableIndex].quantity++;
+         setCurrentInvoiceItemsQueue(updatedInvoiceItemsQueue);
+         handleInvoicePosChange(
+            'total_amount_due',
+            currentInvoiceItemsQueue.reduce(
+               (acc, item) => acc + (item.total_price ?? 0),
+               0,
+            ),
+         );
+      } else {
+         setCurrentInvoiceItemsQueue([
+            ...currentInvoiceItemsQueue,
+            {
+               product: {
+                  id: item.product?.id,
+                  name: item.product?.name,
+                  serial_no: item.product?.serial_no,
+                  brand: item.product?.brand,
+                  size: item.product?.size,
+                  color: item.product?.color,
+               },
+               product_price_id: item.product_price_id,
+               product_price: item.product_price,
+               quantity: item.quantity,
+               unit: item.unit,
+               item_discount: item.item_discount,
+               discount_approval_status: item.discount_approval_status,
+               approved_by: item.approved_by,
+               total_price: item.total_price,
+               source_inventory: item.source_inventory,
+            },
+         ]);
+         handleInvoicePosChange(
+            'total_amount_due',
+            currentInvoiceItemsQueue.reduce(
+               (acc, item) => acc + (item.total_price ?? 0),
+               0,
+            ),
+         );
+      }
+   }
 
    function handleInvoicePosChange(
       key: string,
@@ -92,7 +176,7 @@ export const InvoicePosProvider = ({ children }: InvoiceProviderProps) => {
          handleInvoicePosChange(
             'total_amount_due',
             currentInvoiceItemsQueue.reduce(
-               (acc, item) => acc + item.total_price,
+               (acc, item) => acc + (item.total_price ?? 0),
                0,
             ),
          );
@@ -115,6 +199,7 @@ export const InvoicePosProvider = ({ children }: InvoiceProviderProps) => {
       currentInvoiceItemsQueue,
       setCurrentInvoiceItemsQueue,
 
+      addInvoiceItems,
       handleInvoiceItemQuantiy,
       handleInvoicePosChange,
       removeInvoiceItem,
