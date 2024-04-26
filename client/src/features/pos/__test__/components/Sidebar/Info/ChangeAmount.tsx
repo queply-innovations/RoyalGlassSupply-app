15dce4ff9247bff778dcff590982cd1af35e24f0
gabby <gabbypaylaga@gmail.com>
2024-04-27 03:35:52 +0800

    refactor: change context into useInvoicePos

--- client/src/features/pos/__test__/components/Sidebar/Info/ChangeAmount.tsx
+++ client/src/features/pos/__test__/components/Sidebar/Info/ChangeAmount.tsx
@@ -1,35 +1,38 @@
 import { Label } from '@/components/ui/label';
-import { useInvoice } from '@/features/invoice/__test__/context/InvoiceContext';
 import { formatCurrency } from '@/utils/FormatCurrency';
 import { PaidAmount } from './PaidAmount';
 import { PaymentType } from './PaymentType';
+import { useInvoicePos } from '../../../context/__test__/InvoicePosContext';
 
 export const ChangeAmount = () => {
-	const { invoiceItemsQueue, invoice } = useInvoice();
-	function getChange() {
-		if (invoice.paid_amount === 0) {
-			return 0;
-		} else {
-			return (
-				(invoice.paid_amount ?? 0) -
-				invoiceItemsQueue.reduce((acc, item) => acc + item.total_price, 0) +
-				(invoice.total_discount ?? 0)
-			);
-		}
-	}
-	return (
-		<>
-			<div className="flex flex-col justify-between w-full gap-2 py-2 border-t border-dashed">
-				<PaymentType />
-				<PaidAmount />
+   const { currentInvoiceItemsQueue, currentInvoicePos } = useInvoicePos();
+   function getChange() {
+      if (currentInvoicePos.paid_amount === 0) {
+         return 0;
+      } else {
+         return (
+            (currentInvoicePos.paid_amount ?? 0) -
+            currentInvoiceItemsQueue.reduce(
+               (acc, item) => acc + item.total_price,
+               0,
+            ) +
+            (currentInvoicePos.total_discount ?? 0)
+         );
+      }
+   }
+   return (
+      <>
+         <div className="flex w-full flex-col justify-between gap-2 border-t border-dashed py-2">
+            <PaymentType />
+            <PaidAmount />
 
-				<div className="flex flex-row justify-between">
-					<Label className="text-xl font-bold text-gray-700">Change</Label>
-					<Label className="text-xl font-bold text-black">
-						{formatCurrency(Number(getChange()))}
-					</Label>
-				</div>
-			</div>
-		</>
-	);
+            <div className="flex flex-row justify-between">
+               <Label className="text-xl font-bold text-gray-700">Change</Label>
+               <Label className="text-xl font-bold text-black">
+                  {formatCurrency(Number(getChange()))}
+               </Label>
+            </div>
+         </div>
+      </>
+   );
 };
