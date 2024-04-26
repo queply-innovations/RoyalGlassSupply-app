a476cfe0e647aa22b089d139e954de0b5e5abd96
gabby <gabbypaylaga@gmail.com>
2024-04-27 03:04:39 +0800

    Add sidebar containers for each component

---
+++ client/src/features/pos/__test__/components/Sidebar/Container/index.ts
@@ -0,0 +1,5 @@
+export * from './SidebarContainer';
+export * from './SidebarWarehouseContainer';
+export * from './CustomerInfoContainer';
+export * from './PaymentInfoContainer';
+export * from './DialogButtonsContainer';
