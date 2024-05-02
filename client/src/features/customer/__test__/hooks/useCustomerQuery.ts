df42150e0dd91634f2482dd3949f4518118d3e7f
Gab <gabbypaylaga@gmail.com>
2024-05-03 01:54:41 +0800

    refactor: added search customer by name in context, type and custom hook

--- client/src/features/customer/__test__/hooks/useCustomerQuery.ts
+++ client/src/features/customer/__test__/hooks/useCustomerQuery.ts
@@ -1,25 +1,56 @@
 import { useEffect, useState } from 'react';
 import { Customer } from '../types';
 import { useQuery } from '@tanstack/react-query';
-import { fetchCustomers } from '../api/Customer';
+import { fetchCustomers, searchCustomer } from '../api/Customer';
 
 export const useCustomerQuery = () => {
-	// State of the response data
-	const [data, setData] = useState<Customer[]>([] as Customer[]);
+  // State of the response data
+  const [data, setData] = useState<Customer[]>([] as Customer[]);
 
-	// Query for fetching inventory products and isLoading state
-	const { data: result, isFetching: isLoading } = useQuery({
-		queryKey: ['customers'],
-		queryFn: () => fetchCustomers(),
-		refetchOnWindowFocus: false,
-	});
+  // Query for fetching inventory products and isLoading state
+  const { data: result, isFetching: isLoading } = useQuery({
+    queryKey: ['customers'],
+    queryFn: () => fetchCustomers(),
+    refetchOnWindowFocus: false,
+  });
 
-	// Update states when query results changes [result, isLoading]
-	useEffect(() => {
-		if (!isLoading && result) {
-			setData(result);
-		}
-	}, [result, isLoading]);
+  // Update states when query results changes [result, isLoading]
+  useEffect(() => {
+    if (!isLoading && result) {
+      setData(result);
+    }
+  }, [result, isLoading]);
 
-	return { data, isLoading };
+  return { data, isLoading };
+};
+
+export const useCustomerQueryFullname = (fullName: string) => {
+  const [data, setData] = useState<Customer[]>([]);
+
+  const {
+    data: result,
+    isFetching: isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ['customers'],
+    queryFn: () => {
+      if (fullName !== '') {
+        return searchCustomer(fullName);
+      }
+      return null;
+    },
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (!isLoading && result) {
+      setData(result);
+    }
+  }, [result, isLoading]);
+
+  useEffect(() => {
+    refetch();
+  }, [fullName, refetch]);
+
+  return { data, isLoading };
 };
