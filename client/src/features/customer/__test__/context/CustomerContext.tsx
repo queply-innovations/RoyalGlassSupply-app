26e689aae2d83bcfcf81cd86270d0362cd4acf1b
gabby <gabbypaylaga@gmail.com>
2024-05-04 01:47:58 +0800

    refactor: added customer_id to invoiceContext and removed unused imports

--- client/src/features/customer/__test__/context/CustomerContext.tsx
+++ client/src/features/customer/__test__/context/CustomerContext.tsx
@@ -1,9 +1,13 @@
-import { ReactNode, createContext, useContext, useState } from 'react';
-import { Customer } from '../types';
 import {
-  useCustomerQuery,
-  useCustomerQueryFullname,
-} from '../hooks/useCustomerQuery';
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { Customer } from '../types';
+import { useCustomerQueryFullname } from '../hooks/useCustomerQuery';
+import { useInvoicePos } from '@/features/pos/__test__/context/__test__/InvoicePosContext';
 
 interface CustomerContextProps {
   data: Customer[];
@@ -27,9 +31,19 @@ export const CustomerProvider = ({ children }: CustomerProviderProps) => {
   const [searchFullName, setSearchFullName] = useState('');
   const { data, isLoading } = useCustomerQueryFullname(searchFullName);
 
+  const { currentInvoicePos, setCurrentInvoicePos } = useInvoicePos();
+
   const [selectedCustomer, setSelectedCustomer] = useState<Customer>(
     {} as Customer,
   );
+  useEffect(() => {
+    if (Object.keys(selectedCustomer).length !== 0) {
+      setCurrentInvoicePos({
+        ...currentInvoicePos,
+        customer_id: selectedCustomer.id,
+      });
+    }
+  }, [selectedCustomer]);
   const [openSearchCustomer, setOpenSearchCustomer] = useState(false);
 
   const value = {
