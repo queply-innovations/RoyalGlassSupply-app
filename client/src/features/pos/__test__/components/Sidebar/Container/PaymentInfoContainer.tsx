8317721a79a08b184c1c1b5ed43efbc547b25a4f
Gab <gabbypaylaga@gmail.com>
2024-05-03 22:41:17 +0800

    refactor: removed tax from POS sidebar

--- client/src/features/pos/__test__/components/Sidebar/Container/PaymentInfoContainer.tsx
+++ client/src/features/pos/__test__/components/Sidebar/Container/PaymentInfoContainer.tsx
@@ -1,27 +1,27 @@
 import {
-   DeliveryCharge,
-   Discount,
-   Items,
-   Subtotal,
-   Tax,
-   TotalAmountDue,
-   ChangeAmount,
+	DeliveryCharge,
+	Discount,
+	Items,
+	Subtotal,
+	// Tax,
+	TotalAmountDue,
+	ChangeAmount,
 } from '../Info';
 
 export const PaymentInfoContainer = () => {
-   return (
-      <>
-         <div className="flex h-full flex-col justify-between p-4 px-2">
-            <div className="flex flex-col gap-4">
-               <Items />
-               <Subtotal />
-               <Discount />
-               <Tax />
-               <DeliveryCharge />
-               <TotalAmountDue />
-               <ChangeAmount />
-            </div>
-         </div>
-      </>
-   );
+	return (
+		<>
+			<div className="flex h-full flex-col justify-between p-4 px-2">
+				<div className="flex flex-col gap-4">
+					<Items />
+					<Subtotal />
+					<Discount />
+					{/* <Tax /> */}
+					<DeliveryCharge />
+					<TotalAmountDue />
+					<ChangeAmount />
+				</div>
+			</div>
+		</>
+	);
 };
