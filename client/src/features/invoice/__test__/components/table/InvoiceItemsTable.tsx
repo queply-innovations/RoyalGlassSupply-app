064732774b9f3ab0b7bc5e8c0b2fd307b339372b
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-05-14 15:34:06 +0800

    refactor: changed InvoiceItemsTable component to format product price in POS

--- client/src/features/invoice/__test__/components/table/InvoiceItemsTable.tsx
+++ client/src/features/invoice/__test__/components/table/InvoiceItemsTable.tsx
@@ -4,63 +4,70 @@ import { PosTable } from '@/features/pos/__test__/components/Table/PosTable';
 import { DataTable } from '@/components/Tables/DataTable';
 
 interface InvoiceItemsTableProps {
-   items: InvoiceItemDatabase[];
+	items: InvoiceItemDatabase[];
 }
 
 export const InvoiceItemsTable = ({ items }: InvoiceItemsTableProps) => {
-   const colums: ColumnDef<InvoiceItemDatabase>[] = [
-      {
-         id: 'orderItem',
-         enableResizing: false,
-         header: () => {
-            return <div className="flex ">Item #</div>;
-         },
-         cell: ({ row }) => {
-            return (
-               <div className="flex flex-row gap-2 ">
-                  {row.original.product.brand ? (
-                     <>
-                        <span>({row.original.product.brand})</span>
-                     </>
-                  ) : null}
-                  <span className="font-bold">{row.original.product.name}</span>
-                  <span className="">{row.original.product.size}</span>
-                  <span className="">{row.original.product.color}</span>
-               </div>
-            );
-         },
-         size: 200,
-      },
-      {
-         id: 'quantity',
-         enableResizing: false,
-         header: () => {
-            return <div className="flex ">Quantity</div>;
-         },
-         cell: ({ row }) => {
-            return <div className="flex ">{row.original.quantity}</div>;
-         },
-      },
-      {
-         id: 'price',
-         enableResizing: false,
-         header: () => {
-            return <div className="flex ">Price</div>;
-         },
-         cell: ({ row }) => {
-            return <div className="flex ">{row.original.product_price}</div>;
-         },
-      },
-   ];
-   return (
-      <>
-         <DataTable
-            columns={colums}
-            data={items}
-            dataType="orderItem"
-            filterWhat="price"
-            hideFilter
-         />
-      </>
-   );
+	const colums: ColumnDef<InvoiceItemDatabase>[] = [
+		{
+			id: 'orderItem',
+			enableResizing: false,
+			header: () => {
+				return <div className="flex ">Item #</div>;
+			},
+			cell: ({ row }) => {
+				return (
+					<div className="flex flex-row gap-2 ">
+						{row.original.product.brand ? (
+							<>
+								<span>({row.original.product.brand})</span>
+							</>
+						) : null}
+						<span className="font-bold">{row.original.product.name}</span>
+						<span className="">{row.original.product.size}</span>
+						<span className="">{row.original.product.color}</span>
+					</div>
+				);
+			},
+			size: 200,
+		},
+		{
+			id: 'quantity',
+			enableResizing: false,
+			header: () => {
+				return <div className="flex ">Quantity</div>;
+			},
+			cell: ({ row }) => {
+				return <div className="flex ">{row.original.quantity}</div>;
+			},
+		},
+		{
+			id: 'price',
+			enableResizing: false,
+			header: () => {
+				return <div className="flex ">Price</div>;
+			},
+			cell: ({ row }) => {
+				return (
+					<div className="flex ">
+						{Intl.NumberFormat('en-PH', {
+							style: 'currency',
+							currency: 'PHP',
+						}).format(row.original.product_price ?? 0)}
+					</div>
+				);
+			},
+		},
+	];
+	return (
+		<>
+			<DataTable
+				columns={colums}
+				data={items}
+				dataType="orderItem"
+				filterWhat="price"
+				hideFilter
+			/>
+		</>
+	);
 };
