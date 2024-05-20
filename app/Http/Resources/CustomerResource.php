fb6a001cd8e3fd2aab3c14d8f4b7b6c8b2a20308
Neil Francis Britos <neilexfrancis18@gmail.com>
2024-05-20 20:57:08 +0800

    - vouchers on customer data

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -21,7 +21,8 @@ class CustomerResource extends JsonResource
             'address' => $this->address,
             'contact_no' => $this->contact_no,
             'total_credit' => $this->vouchers->sum('discounted_price'),
-            'total_balance' => $this->invoices->sum('balance_amount')
+            'total_balance' => $this->invoices->sum('balance_amount'),
+            'vouchers' => $this->vouchers
         ];
     }
 }
