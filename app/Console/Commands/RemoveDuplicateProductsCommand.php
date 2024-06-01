c30e61cb0f77640667aa058aa120f497b6b3953f
wendalefranz.dy <wendalefranz.dy@gmail.com>
2024-06-01 19:21:53 +0800

    Update - added command file to remove duplicates

---
+++ app/Console/Commands/RemoveDuplicateProductsCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Product;
+
+class RemoveDuplicateProductsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:remove-duplicate-products-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try{
+            $ids = Product::groupBy(['serial_no', 'name', 'brand', 'size', 'color'])
+            ->get(['id'])
+            ->all();
+            Product::whereNotIn('id', $ids)->delete();
+        } catch(\Exception $e){
+            Log::info($e);
+        }
+
+    }
+    
+}
