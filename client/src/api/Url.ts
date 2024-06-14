39b24b5b07aac7ba08c8798831443dcb48007aa9
Neil Francis Britos <neilexfrancis18@gmail.com>
2024-06-14 12:38:48 +0800

    Build staging web app

--- client/src/api/Url.ts
+++ client/src/api/Url.ts
@@ -1,6 +1,6 @@
 import storage from '@/utils/storage';
 
-export const API_BASE_URL = 'https://royalglasssupply.com/api';
+export const API_BASE_URL = 'https://staging.royalglasssupply.com/api';
 
 export const API_URLS = {
 	PRODUCTS: `${API_BASE_URL}/products`,
