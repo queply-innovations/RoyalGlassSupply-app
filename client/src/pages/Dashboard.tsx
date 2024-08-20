2acfe998f622c2285dc277ecf4c4b0868d7e7a6b
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-08-21 07:03:37 +0800

    refactor: Update Dashboard component to conditionally display content based on user role

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import TransferStatus from '@/components/Tables/Transfer/Transfer';
-import { Navigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 import { DashboardReportsProvider } from '@/features/dashboard';
 import {
@@ -12,24 +11,15 @@ import {
 import { MainLayout } from '@/layouts/MainLayout';
 import { ReportsAnalytics } from '@/features/dashboard/components';
 import { TopSellingProducts } from '@/features/dashboard/components/TopSellingProducts';
-import { InventoryLevelReport } from '@/features/dashboard/components/InventoryLevelReport';
-
-// interface DashboardProps {
-// 	state: Array<unknown>;
-// }
+import { LayoutDashboard } from 'lucide-react';
 
 export const Dashboard = () => {
 	const { auth } = useAuth();
 
-	// // Redirect to POS if user role includes 'encoder' or 'sales'
-	// if (auth.role?.includes('encoder') || auth.role?.includes('sales')) {
-	// 	return <Navigate to="/pos" />;
-	// }
-
 	return (
 		<MainLayout title="Dashboard">
 			<DashboardReportsProvider>
-				{auth.role?.includes('admin') && (
+				{auth.role?.includes('admin') ? (
 					<>
 						<div className="flex h-full w-full flex-1 flex-col gap-5 rounded-xl">
 							<div className="flex h-full max-h-full w-full max-w-full flex-col gap-4 overflow-auto">
@@ -46,13 +36,25 @@ export const Dashboard = () => {
 										<TransferStatus />
 									</div>
 									<div className="row-container flex h-[480px] flex-row justify-between gap-4">
-										{/* <InventoryLevelReport /> */}
 										<TopSellingProducts />
 									</div>
 								</div>
 							</div>
 						</div>
 					</>
+				) : (
+					<div className="flex h-full w-full items-center justify-center">
+						<div className="flex flex-col items-center gap-6">
+							<LayoutDashboard
+								strokeWidth={2}
+								size={142}
+								className="text-slate-700/30"
+							/>
+							<p className="text-slate-700/60">
+								Welcome to dashboard. Navigate to the items on the left.
+							</p>
+						</div>
+					</div>
 				)}
 			</DashboardReportsProvider>
 		</MainLayout>
