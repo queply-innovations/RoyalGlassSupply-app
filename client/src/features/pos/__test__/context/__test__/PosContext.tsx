f44c7ee26804959a20a4143b7b95c4b34877b6cd
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-09-04 03:19:11 +0800

    refactor: Update PosContext warehouse filtering
    - Modify PosContext component to conditionally set the warehouse_id filter based on the user's role
    - Update the searchFilterItems state to include the warehouse_id filter
    - Update the setCurrentInvoicePos state to include the warehouse_id value
    - Navigate to the appropriate route based on the user's role
    - Remove redundant state management in useProductPricesQuery and used isFetching instead of isLoading

--- client/src/features/pos/__test__/context/__test__/PosContext.tsx
+++ client/src/features/pos/__test__/context/__test__/PosContext.tsx
@@ -1,105 +1,105 @@
 import { useAuth } from '@/context/AuthContext';
 import { InvoiceItems } from '@/features/invoice/__test__/types';
 import {
-   ProductPricesFilterProps,
-   useProductPricesFilter,
+	ProductPricesFilterProps,
+	useProductPricesFilter,
 } from '@/features/product/__test__/hooks';
 import { ProductPrices } from '@/features/product/__test__/types';
 import {
-   ReactNode,
-   createContext,
-   useContext,
-   useEffect,
-   useState,
+	ReactNode,
+	createContext,
+	useContext,
+	useEffect,
+	useState,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useInvoicePos } from './InvoicePosContext';
 
 interface PosContextProps {
-   searchFilterItems: ProductPricesFilterProps;
-   setSearchFilterItems: React.Dispatch<
-      React.SetStateAction<ProductPricesFilterProps>
-   >;
+	searchFilterItems: ProductPricesFilterProps;
+	setSearchFilterItems: React.Dispatch<
+		React.SetStateAction<ProductPricesFilterProps>
+	>;
 
-   sellableItems: ProductPrices[];
-   fetchingProducts: boolean;
+	sellableItems: ProductPrices[] | null | undefined;
+	fetchingProducts: boolean;
 
-   customerCart: InvoiceItems[];
-   setCustomerCart: React.Dispatch<React.SetStateAction<InvoiceItems[]>>;
+	customerCart: InvoiceItems[];
+	setCustomerCart: React.Dispatch<React.SetStateAction<InvoiceItems[]>>;
 
-   dialogOptions: DialogOptions;
-   setDialogOptions: React.Dispatch<React.SetStateAction<DialogOptions>>;
+	dialogOptions: DialogOptions;
+	setDialogOptions: React.Dispatch<React.SetStateAction<DialogOptions>>;
 }
 
 interface PosProviderProps {
-   children: ReactNode;
+	children: ReactNode;
 }
 
 interface DialogOptions {
-   open: boolean;
-   title: string;
+	open: boolean;
+	title: string;
 }
 
 const PosContext = createContext<PosContextProps | undefined>(undefined);
 
 export const PosProvider = ({ children }: PosProviderProps) => {
-   const { auth } = useAuth();
-   const navigate = useNavigate();
+	const { auth } = useAuth();
+	const navigate = useNavigate();
 
-   const { currentInvoicePos, setCurrentInvoicePos } = useInvoicePos();
+	const { currentInvoicePos, setCurrentInvoicePos } = useInvoicePos();
 
-   const [searchFilterItems, setSearchFilterItems] =
-      useState<ProductPricesFilterProps>({});
+	const [searchFilterItems, setSearchFilterItems] =
+		useState<ProductPricesFilterProps>({});
 
-   const [customerCart, setCustomerCart] = useState<InvoiceItems[]>([]);
+	const [customerCart, setCustomerCart] = useState<InvoiceItems[]>([]);
 
-   const [dialogOptions, setDialogOptions] = useState<DialogOptions>(
-      {} as DialogOptions,
-   );
+	const [dialogOptions, setDialogOptions] = useState<DialogOptions>(
+		{} as DialogOptions,
+	);
 
-   useEffect(() => {
-      if (auth.role === 'admin') {
-         navigate('/pos');
-      } else if (auth.role?.split('_')[1] === 'CDO') {
-         setSearchFilterItems({
-            // approval_status: 'approved', //TODO Possible to comment out
-            warehouse_id: 1,
-         });
-         setCurrentInvoicePos({ ...currentInvoicePos, warehouse_id: 1 });
-         navigate('/pos/add-order');
-      } else if (auth.role?.split('_')[1] === 'ILI') {
-         setSearchFilterItems({
-            // approval_status: 'approved', //TODO Possible to comment out
-            warehouse_id: 2,
-         });
-         setCurrentInvoicePos({ ...currentInvoicePos, warehouse_id: 2 });
-         navigate('/pos/add-order');
-      }
-   }, [auth.role]);
+	useEffect(() => {
+		if (auth.role === 'admin') {
+			navigate('/pos');
+		} else if (auth.role?.split('_')[1].toLowerCase() === 'cdo') {
+			setSearchFilterItems({
+				// approval_status: 'approved', //TODO Possible to comment out
+				warehouse_id: 1,
+			});
+			setCurrentInvoicePos({ ...currentInvoicePos, warehouse_id: 1 });
+			navigate('/pos/add-order');
+		} else if (auth.role?.split('_')[1].toLowerCase() === 'ili') {
+			setSearchFilterItems({
+				// approval_status: 'approved', //TODO Possible to comment out
+				warehouse_id: 2,
+			});
+			setCurrentInvoicePos({ ...currentInvoicePos, warehouse_id: 2 });
+			navigate('/pos/add-order');
+		}
+	}, [auth.role]);
 
-   const { data: sellableItems, isLoading: fetchingProducts } =
-      useProductPricesFilter(searchFilterItems);
+	const { data: sellableItems, isFetching: fetchingProducts } =
+		useProductPricesFilter(searchFilterItems);
 
-   const value = {
-      searchFilterItems,
-      setSearchFilterItems,
+	const value = {
+		searchFilterItems,
+		setSearchFilterItems,
 
-      sellableItems,
-      fetchingProducts,
+		sellableItems,
+		fetchingProducts,
 
-      customerCart,
-      setCustomerCart,
+		customerCart,
+		setCustomerCart,
 
-      dialogOptions,
-      setDialogOptions,
-   };
-   return <PosContext.Provider value={value}>{children}</PosContext.Provider>;
+		dialogOptions,
+		setDialogOptions,
+	};
+	return <PosContext.Provider value={value}>{children}</PosContext.Provider>;
 };
 
 export const usePos = () => {
-   const context = useContext(PosContext);
-   if (context === undefined) {
-      throw new Error('usePos must be used within a PosProvider');
-   }
-   return context;
+	const context = useContext(PosContext);
+	if (context === undefined) {
+		throw new Error('usePos must be used within a PosProvider');
+	}
+	return context;
 };
--- client/src/features/product/__test__/hooks/useProductPricesQuery.ts
+++ client/src/features/product/__test__/hooks/useProductPricesQuery.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import {
-   fetchApprovedProductPrices,
-   fetchPendingProductPrices,
-   fetchProductPrices,
-   fetchProductPricesFilters,
+	fetchApprovedProductPrices,
+	fetchPendingProductPrices,
+	fetchProductPrices,
+	fetchProductPricesFilters,
 } from '../api/Products';
 import { useEffect, useState } from 'react';
 import { ProductPrices } from '../types';
@@ -14,107 +14,76 @@ import { ProductPrices } from '../types';
  * @returns An object containing the response data and loading state.
  */
 export const useProductPricesQuery = () => {
-   // State of the response data
-   const [data, setData] = useState<ProductPrices[]>([] as ProductPrices[]);
+	// State of the response data
+	const [data, setData] = useState<ProductPrices[]>([] as ProductPrices[]);
 
-   // Query for fetching product prices and isLoading state
-   const { data: result, isFetching: isLoading } = useQuery({
-      queryKey: ['productPrices'],
-      queryFn: () => fetchProductPrices(),
-      refetchOnWindowFocus: false,
-   });
+	// Query for fetching product prices and isLoading state
+	const { data: result, isFetching: isLoading } = useQuery({
+		queryKey: ['productPrices'],
+		queryFn: () => fetchProductPrices(),
+		refetchOnWindowFocus: false,
+	});
 
-   // Update states when query results changes [result, isLoading]
-   useEffect(() => {
-      if (!isLoading && result) {
-         setData(result);
-      }
-   }, [result, isLoading]);
+	// Update states when query results changes [result, isLoading]
+	useEffect(() => {
+		if (!isLoading && result) {
+			setData(result);
+		}
+	}, [result, isLoading]);
 
-   return { data, isLoading };
+	return { data, isLoading };
 };
 
 export const usePendingProductPricesQuery = () => {
-   // State of the response data
-   const [data, setData] = useState<ProductPrices[]>([] as ProductPrices[]);
+	// State of the response data
+	const [data, setData] = useState<ProductPrices[]>([] as ProductPrices[]);
 
-   // Query for fetching product prices and isLoading state
-   const { data: result, isFetching: isLoading } = useQuery({
-      queryKey: ['pendingProductPrices'],
-      queryFn: () => fetchPendingProductPrices(),
-      refetchOnWindowFocus: false,
-   });
+	// Query for fetching product prices and isLoading state
+	const { data: result, isFetching: isLoading } = useQuery({
+		queryKey: ['pendingProductPrices'],
+		queryFn: () => fetchPendingProductPrices(),
+		refetchOnWindowFocus: false,
+	});
 
-   // Update states when query results changes [result, isLoading]
-   useEffect(() => {
-      if (!isLoading && result) {
-         setData(result);
-      }
-   }, [result, isLoading]);
+	// Update states when query results changes [result, isLoading]
+	useEffect(() => {
+		if (!isLoading && result) {
+			setData(result);
+		}
+	}, [result, isLoading]);
 
-   return { data, isLoading };
+	return { data, isLoading };
 };
 export const useProductPricesQueryFilterByApproved = () => {
-   const [data, setData] = useState<ProductPrices[]>([] as ProductPrices[]);
+	// Query for fetching product prices and isLoading state
+	const { data, isFetching: isLoading } = useQuery({
+		queryKey: ['approvedProductPrices'],
+		queryFn: () => fetchApprovedProductPrices(),
+		refetchOnWindowFocus: false,
+	});
 
-   // Query for fetching product prices and isLoading state
-   const { data: result, isFetching: isLoading } = useQuery({
-      queryKey: ['approvedProductPrices'],
-      queryFn: () => fetchApprovedProductPrices(),
-      refetchOnWindowFocus: false,
-   });
-
-   // Update states when query results changes [result, isLoading]
-   useEffect(() => {
-      if (!isLoading && result) {
-         setData(result);
-      }
-   }, [result, isLoading]);
-
-   return { data, isLoading };
+	return { data, isLoading };
 };
 
 export interface ProductPricesFilterProps {
-   approval_status?: string;
-   warehouse_id?: number;
+	approval_status?: string;
+	warehouse_id?: number;
 }
 
 export const useProductPricesFilter = ({
-   // approval_status,
-   warehouse_id,
+	warehouse_id,
 }: ProductPricesFilterProps) => {
-   const [data, setData] = useState<ProductPrices[]>([]);
-   const {
-      data: result,
-      isLoading,
-      refetch,
-   } = useQuery({
-      queryKey: ['FilteredProductPrices'],
-      queryFn: () => {
-         if (warehouse_id) {
-            return fetchProductPricesFilters(warehouse_id);
-         }
-         // if (approval_status && warehouse_id) {
-         // 	return fetchProductPricesFilters(approval_status, warehouse_id);
-         // }
-         // Return a placeholder or null if any of the required parameters are missing
-         return null;
-      },
-      refetchOnWindowFocus: false,
-   });
-   useEffect(() => {
-      if (!isLoading && result) {
-         setData(result);
-      }
-   }, [result, isLoading]);
+	const { data, isFetching } = useQuery({
+		queryKey: ['FilteredProductPrices', warehouse_id],
+		queryFn: () => {
+			if (warehouse_id) {
+				return fetchProductPricesFilters(warehouse_id);
+			}
+			// Return a placeholder or null if any of the required parameters are missing
+			return null;
+		},
+		refetchOnWindowFocus: false,
+	});
 
-   // Refetch data when any of the filter values change
-   // useEffect(() => {
-   //    refetch();
-   // }, [approval_status, warehouse_id, refetch]);
-   useEffect(() => {
-      refetch();
-   }, [warehouse_id, refetch]);
-
-   return { data, isLoading };
+	return { data, isFetching };
 };
