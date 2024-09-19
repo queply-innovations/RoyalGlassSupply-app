0e85d3fd5d285e1a73b25dcdcb1ee003ce9f417f
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-09-20 01:04:39 +0800

    refactor: Revert to previous product prices table to use search/filter function

--- client/src/features/product/__test__/components/table/ProductPricesTable.tsx
+++ client/src/features/product/__test__/components/table/ProductPricesTable.tsx
@@ -1,47 +1,75 @@
-import { ProductPricesPOS } from '../../types';
+import { Product, ProductPrices, ProductPricesPOS } from '../../types';
 import { ProductPricesColumns } from '.';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { DataTablePagination } from '@/components/Tables/DataTablePagination';
-import { useProductPricesPaginated } from '../../context/ProductPricesPaginatedContext';
+// import { DataTablePagination } from '@/components/Tables/DataTablePagination';
+// import { useProductPricesPaginated } from '../../context/ProductPricesPaginatedContext';
+import { DataTable } from '@/components/Tables/DataTable';
+import { useProductPrices } from '../../context/ProductPricesContext';
 
 interface ProductsPricesTableProps {
-	openModal: (action: string) => void;
+	openModal: (data: ProductPrices | Product, action: string) => void;
+	isModalOpen: boolean;
+	filterWarehouse?: number;
 }
 
-export const ProductPricesTable = ({ openModal }: ProductsPricesTableProps) => {
-	const {
-		data: paginatedData,
-		isLoading,
-		isFetching,
-		pagination,
-		setPagination,
-		setSelectedProductPrice,
-		sorting,
-		setSorting,
-	} = useProductPricesPaginated();
+export const ProductPricesTable = ({
+	openModal,
+	isModalOpen,
+	filterWarehouse,
+}: ProductsPricesTableProps) => {
+	// const {
+	// 	data: paginatedData,
+	// 	isLoading,
+	// 	isFetching,
+	// 	pagination,
+	// 	setPagination,
+	// 	setSelectedProductPrice,
+	// 	sorting,
+	// 	setSorting,
+	// } = useProductPricesPaginated();
+
+	// // Modal handler to expand product pricing/listing details
+	// const handleProdPriceDetails = (productPrice: ProductPricesPOS) => {
+	// 	setSelectedProductPrice(productPrice);
+	// 	openModal('details');
+	// };
+
+	// // Modal handler to edit product pricing/listing
+	// const handleEditProdPrice = (productPrice: ProductPricesPOS) => {
+	// 	setSelectedProductPrice(productPrice);
+	// 	openModal('edit');
+	// };
+
+	// // Modal handler to delete product pricing/listing
+	// const handleDeleteProdPrice = (productPrice: ProductPricesPOS) => {
+	// 	setSelectedProductPrice(productPrice);
+	// 	openModal('delete');
+	// };
+
+	const { data, isLoading, setSelectedProductPrice } = useProductPrices();
 
 	// Modal handler to expand product pricing/listing details
-	const handleProdPriceDetails = (productPrice: ProductPricesPOS) => {
+	const handleProdPriceDetails = (productPrice: ProductPrices) => {
 		setSelectedProductPrice(productPrice);
-		openModal('details');
+		openModal(productPrice, 'details');
 	};
 
 	// Modal handler to edit product pricing/listing
-	const handleEditProdPrice = (productPrice: ProductPricesPOS) => {
+	const handleEditProdPrice = (productPrice: ProductPrices) => {
 		setSelectedProductPrice(productPrice);
-		openModal('edit');
+		openModal(productPrice, 'edit');
 	};
 
 	// Modal handler to delete product pricing/listing
-	const handleDeleteProdPrice = (productPrice: ProductPricesPOS) => {
+	const handleDeleteProdPrice = (productPrice: ProductPrices) => {
 		setSelectedProductPrice(productPrice);
-		openModal('delete');
+		openModal(productPrice, 'delete');
 	};
 
 	return (
 		<>
 			<TooltipProvider>
-				<DataTablePagination
+				{/* <DataTablePagination
 					data={paginatedData?.data ?? []}
 					columns={ProductPricesColumns({
 						handleProdPriceDetails,
@@ -57,6 +85,40 @@ export const ProductPricesTable = ({ openModal }: ProductsPricesTableProps) => {
 					from={paginatedData?.from}
 					to={paginatedData?.to}
 					isFetching={isFetching}
+					isLoading={isLoading}
+				/> */}
+
+				<DataTable
+					columns={ProductPricesColumns({
+						handleProdPriceDetails,
+						handleEditProdPrice,
+						handleDeleteProdPrice,
+					})}
+					data={
+						filterWarehouse
+							? data
+									.filter(
+										item => item.warehouse.id === filterWarehouse,
+									)
+									.sort((a, b) => {
+										const dateA = new Date(
+											a.updated_at ?? a.created_at,
+										);
+										const dateB = new Date(
+											b.updated_at ?? b.created_at,
+										);
+
+										return dateB.getTime() - dateA.getTime();
+									})
+							: data.sort((a, b) => {
+									const dateA = new Date(a.updated_at ?? a.created_at);
+									const dateB = new Date(b.updated_at ?? b.created_at);
+
+									return dateB.getTime() - dateA.getTime();
+								})
+					}
+					filterWhat={'name'}
+					dataType={'Listing'}
 					isLoading={isLoading}
 				/>
 			</TooltipProvider>
