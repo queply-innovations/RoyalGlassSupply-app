c25bc240607d6406c6f19ecc71757779d776b69c
Neil Francis Britos <neilexfrancis18@gmail.com>
2024-09-25 22:15:47 +0800

    UPDATES - Fix balance repayment not adding to sales

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -14,6 +14,10 @@ class CustomerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $balancePayment = $this->invoices->where('payment_method', 'balance_payment')->sum('paid_amount') - $this->invoices->where('payment_method', 'balance_payment')->sum('change_amount');
+        $sales = $this->invoices->whereNotIn('payment_method', ['balance_payment', 'purchase_order'])->where('is_paid', true)->sum('total_amount_due');
+        $totalSales = $balancePayment + $sales;
+
         return [
             'id' => $this->id,
             'firstname' => $this->firstname,
@@ -22,7 +26,7 @@ class CustomerResource extends JsonResource
             'contact_no' => $this->contact_no,
             'total_credit' => $this->vouchers->sum('discounted_price'),
             'total_balance' => $this->invoices->sum('balance_amount'),
-            'total_sales' => round($this->invoices->whereNotIn('payment_method', ['balance_payment', 'purchase_order'])->where('is_paid', true)->sum('total_amount_due'), 2),
+            'total_sales' => round($totalSales, 2),
         ];
     }
 }
