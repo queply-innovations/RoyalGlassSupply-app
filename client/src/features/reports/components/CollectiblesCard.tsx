663a09f50a1582fde65fb3ad5ae7862c3cf90a89
Blaire Mendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2024-09-27 20:57:32 +0800

    refactor: Update CollectiblesCard to display total overall collectibles
    - Updated the CollectiblesCard component in both the dashboard and reports features to display the total overall collectibles owed from customers for non-payment type transactions.
    - The component now uses the `total_overall_collectibles` property from the `reports` object.

--- client/src/features/reports/components/CollectiblesCard.tsx
+++ client/src/features/reports/components/CollectiblesCard.tsx
@@ -2,26 +2,27 @@ import { useReportsContext } from '../context/ReportsContext';
 import { ReportCard } from './ReportCard';
 
 export const CollectiblesCard = () => {
-   const { reports, isReportsFetching } = useReportsContext();
+	const { reports, isReportsFetching } = useReportsContext();
 
-   return (
-      <>
-         <ReportCard
-            title="Collectibles"
-            tooltip="Total amount owed from customers for non-payment type transactions."
-            bgClassName="bg-stone-50"
-            textColorClassName="text-stone-800">
-            {isReportsFetching ? (
-               <div className="h-7 w-full animate-pulse rounded-md bg-stone-700/10"></div>
-            ) : (
-               <p className="text-right text-xl font-semibold tracking-tight">
-                  {Intl.NumberFormat('en-US', {
-                     style: 'currency',
-                     currency: 'PHP',
-                  }).format(reports?.total_collectibles ?? 0)}
-               </p>
-            )}
-         </ReportCard>
-      </>
-   );
+	return (
+		<>
+			<ReportCard
+				title="Collectibles"
+				tooltip="Total amount owed from customers for non-payment type transactions."
+				bgClassName="bg-stone-50"
+				textColorClassName="text-stone-800"
+			>
+				{isReportsFetching ? (
+					<div className="h-7 w-full animate-pulse rounded-md bg-stone-700/10"></div>
+				) : (
+					<p className="text-right text-xl font-semibold tracking-tight">
+						{Intl.NumberFormat('en-US', {
+							style: 'currency',
+							currency: 'PHP',
+						}).format(reports?.total_overall_collectibles ?? 0)}
+					</p>
+				)}
+			</ReportCard>
+		</>
+	);
 };
